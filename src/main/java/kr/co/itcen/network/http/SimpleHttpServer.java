df0a944791bc4030ac100f9376dd49b870197835
BIT <BIT@DESKTOP-ET39DN4>
2019-09-02 09:37:58 +0900

    http 서버

--- src/main/java/kr/co/itcen/network/http/SimpleHttpServer.java
+++ src/main/java/kr/co/itcen/network/http/SimpleHttpServer.java
@@ -6,7 +6,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SimpleHttpServer {
-	private static final int PORT = 8000;
+	private static final int PORT = 8080;
 
 	public static void main(String[] args) {
 
